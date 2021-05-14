0962d514ec4219053622900f4b33a9ff8b5793c4
Vsevolod <aleksandriyskiy99@mail.ru>
2021-05-14 19:29:42 +0300

    add creation todos in admin

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Todo
 # Register your models here.
+class TodoAdmin(admin.ModelAdmin):# добавлениие поля для видимости в админке
+    readonly_fields = ('date_created',)
+
+
+admin.site.register(Todo, TodoAdmin)
