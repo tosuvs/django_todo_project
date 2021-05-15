47e1007c9833d7787f185055a27a0735528e03c2
Vsevolod <aleksandriyskiy99@mail.ru>
2021-05-15 19:06:29 +0300

    added task execution and task deletion

--- todo/views.py
+++ todo/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import login,logout,authenticate
 from .forms import TodoForm
 from .models import Todo
+from django.utils import timezone
 
 def home(request):
     return render(request, 'todo/home.html')
@@ -69,3 +70,16 @@ def viewtodo(request, todo_pk):
             return redirect('currenttodos')
         except ValueError:
             return render(request, 'todo/viewtodo.html', {'todo': todo, 'form': form, 'error': 'Incorrect info'})
+
+def completetodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == 'POST':
+        todo.date_completed = timezone.now()
+        todo.save()
+        return redirect('currenttodos')
+
+def deletetodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == 'POST':
+        todo.delete()
+        return redirect('currenttodos')
